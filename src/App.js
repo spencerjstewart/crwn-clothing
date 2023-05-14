ebd815a590ad34aebc05e7064a05c6e3d420cffa
spencer <spencerjamesstewart@gmail.com>
2023-05-14 13:24:30 -0500

    Add base structure to work from.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,36 @@ const App = () => {
 			<div className="category-container">
 				{/*{<img/>}*/}
 				<div className="category-body-container">
-				
+					<h2>Hats</h2>
+					<p>Shop Now</p>
+				</div>
+			</div>
+			<div className="category-container">
+				{/*{<img/>}*/}
+				<div className="category-body-container">
+					<h2>Jackets</h2>
+					<p>Shop Now</p>
+				</div>
+			</div>
+			<div className="category-container">
+				{/*{<img/>}*/}
+				<div className="category-body-container">
+					<h2>Sneakers</h2>
+					<p>Shop Now</p>
+				</div>
+			</div>
+			<div className="category-container">
+				{/*{<img/>}*/}
+				<div className="category-body-container">
+					<h2>Womens</h2>
+					<p>Shop Now</p>
+				</div>
+			</div>
+			<div className="category-container">
+				{/*{<img/>}*/}
+				<div className="category-body-container">
+					<h2>Mens</h2>
+					<p>Shop Now</p>
 				</div>
 			</div>
 		</div>
